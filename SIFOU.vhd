3a2ff2d454bd5301318febfb5e62d8841f2665ec
Mieartt <ard.iheb@gmail.com>
2025-01-29 12:26:39 +0100

    sifou second commit

--- SIFOU.vhd
+++ SIFOU.vhd
@@ -1,6 +1,6 @@
 library ieee;
 use ieee.std_logic_116.all;
 entity sifou is
-port ( wednin : in std_logic;
-        fom : out std_logic);
+port ( wednin, khcham : in std_logic;
+        fom           : out std_logic);
 end entity;
